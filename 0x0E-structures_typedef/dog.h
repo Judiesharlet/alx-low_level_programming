f09d58a160b3e05e9883efa56e2f7651c899cc00
Judiesharlet <judithnjoki6@gmail.com>
2023-07-19 01:57:36 +0300

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,27 +1,23 @@
-#ifndef DOG_H
-#define DOG_H
+#include "dog.h"
 
 /**
- * struct dog - a derived type describing a dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * _strcpy - copies a string from src to dest
+ * @dest: destination
+ * @src: source
+ *
+ * Return: dest/copied string
  */
 
-struct dog
+char *_strcpy(char *dest, char *src)
 {
-	char *name;
-	float age;
-	char *owner;
-};
+	int i = 0;
 
-typedef struct dog dog_t;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
 
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
-
-#endif
+	return (dest);
+}
